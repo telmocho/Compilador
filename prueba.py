c3200832cb1127114b2006e95cbc0c1f159c3acd
telmocho <telmo.garcia@alu.uclm.es>
2026-09-19 12:54:43 +0200

    Los archivos faltantes se han añadido.

---
+++ prueba.py
@@ -0,0 +1,3 @@
+palabra = "hola"
+
+print(palabra[1])
---
+++ tabla_simbolos.py
@@ -0,0 +1,12 @@
+class TablaSimbolos:
+    def __init__(self):
+        self.diccionario = {}
+
+    def InsertSymbol(self, identificador, valor, tipo, dir_mem):
+        self.diccionario[identificador] = {"valor": valor, "tipo": tipo, "direccion_memoria": dir_mem}
+
+    def ModifySymbol():
+
+    def Exists():
+
+    def GetValue():
